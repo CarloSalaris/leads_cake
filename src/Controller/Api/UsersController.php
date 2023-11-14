eea60b3d46ca5fba591dff5a71b5ac6b4105fd9b
CarloSalaris <carloa.salaris@gmail.com>
2023-11-14 10:45:38 +0100

    Api/users controller

---
+++ src/Controller/Api/UsersController.php
@@ -0,0 +1,144 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use App\Controller\AppController;
+
+/**
+ * Users Controller
+ *
+ * @property \App\Model\Table\UsersTable $users
+ */
+class UsersController extends AppController
+{
+    public function initialize(): void
+    {
+        parent::initialize();
+
+        $this->loadModel("Users");
+    }
+
+    // List Users api
+    public function listUsers()
+    {
+        $this->request->allowMethod(["get"]);
+
+        $users = $this->Users->find()
+        ->contain(['Leads'])
+        ->toList();
+
+        $this->set([
+            "status" => true,
+            "message" => "Users list",
+            "data" => $users
+        ]);
+
+        $this->viewBuilder()->setOption("serialize", ["status", "message", "data"]);
+    }
+
+    // Add User api
+    public function addUser()
+    {
+        $this->request->allowMethod(["post"]);
+
+        // form data
+        $formData = $this->request->getData();
+
+        // insert new User
+        $usersObject = $this->Users->newEmptyEntity();
+
+        $usersObject = $this->Users->patchEntity($usersObject, $formData);
+
+        if ($this->Users->save($usersObject)) {
+            // success response
+            $status = true;
+            $message = "User has been created";
+        } else {
+            // error response
+            $status = false;
+            $message = "Failed to create user";
+        }
+
+        $this->set([
+            "status" => $status,
+            "message" => $message
+        ]);
+
+        $this->viewBuilder()->setOption("serialize", ["status", "message"]);
+    }
+
+    // Update User
+    public function updateUser()
+    {
+        $this->request->allowMethod(["put", "post"]);
+
+        $emp_id = $this->request->getParam("id");
+
+        $userInfo = $this->request->getData();
+
+        // User check
+        $user = $this->Users->get($emp_id);
+
+        if (!empty($user)) {
+            // Users exists
+            $user = $this->Users->patchEntity($user, $userInfo);
+
+            if ($this->Users->save($user)) {
+                // success response
+                $status = true;
+                $message = "User has been updated";
+            } else {
+                // error response
+                $status = false;
+                $message = "Failed to update user";
+            }
+        } else {
+            // User not found
+            $status = false;
+            $message = "User Not Found";
+        }
+
+        $this->set([
+            "status" => $status,
+            "message" => $message
+        ]);
+
+        $this->viewBuilder()->setOption("serialize", ["status", "message"]);
+    }
+
+    // Delete User api
+    public function deleteUser()
+    {
+        $this->request->allowMethod(["delete"]);
+
+        $emp_id = $this->request->getParam("id");
+
+        $user = $this->Users->get($emp_id);
+
+        if (!empty($user)) {
+            // User found
+            if ($this->Users->delete($user)) {
+                // User deleted
+                $status = true;
+                $message = "User has been deleted";
+            } else {
+                // failed to delete
+                $status = false;
+                $message = "Failed to delete user";
+            }
+        } else {
+            // not found
+            $status = false;
+            $message = "User doesn't exists";
+        }
+
+        $this->set([
+            "status" => $status,
+            "message" => $message
+        ]);
+
+        $this->viewBuilder()->setOption("serialize", ["status", "message"]);
+    }
+
+}
