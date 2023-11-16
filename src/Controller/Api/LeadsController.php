721a4cd1962afe4406a2cb62aa3006989d96b98d
CarloSalaris <carloa.salaris@gmail.com>
2023-11-16 18:10:15 +0100

    Leads controller refactoring

--- src/Controller/Api/LeadsController.php
+++ src/Controller/Api/LeadsController.php
@@ -20,14 +20,58 @@ class LeadsController extends AppController
         $this->loadModel("Leads");
     }
 
-    // List leads api
     public function index()
     {
         $this->request->allowMethod(["get"]);
 
+        $elements = $this->getElements(['Users', 'Clients', 'LeadOffers']);
+
+        $this->response($elements);
+    }
+
+    public function view($id)
+    {
+        $this->request->allowMethod(["get"]);
+
+        $element = $this->getElement($id, ['contain' => ['Users', 'Clients', 'LeadOffers']]);
+
+        $this->response($element);
+    }
+    public function add()
+    {
+        $this->request->allowMethod(["post"]);
+
+        $element = $this->form();
+
+        $this->response($element);
+    }
+
+    public function edit($id)
+    {
+        $this->request->allowMethod(["put", "post"]);
+
+        $element = $this->form($id);
+
+        $this->response($element);
+    }
+
+    public function delete($id)
+    {
+        $this->request->allowMethod(["delete"]);
+
+        $element = $this->getElement($id);
+
+        $this->Leads->delete($element);
+
+        $this->response($element);
+    }
+
+    //REUSABLE METHODS
+    protected function getElements($options)
+    {
         $q = $this->Leads
         ->find()
-        ->contain(['Users', 'Clients', 'LeadOffers']);
+        ->contain($options);
 
         if ($this->request->getQuery('notClient')) {
             $q->find('notClient');
@@ -38,144 +82,29 @@ class LeadsController extends AppController
         if ($this->request->getQuery('privato')) {
             $q->find('privato');
         }
-        $leads = $q->toList();
-
-        $this->set([
-            "status" => true,
-            "message" => "Leads list",
-            "data" => $leads,
-        ]);
-
-        $this->viewBuilder()->setOption("serialize", ["status", "message", "data"]);
+        return $q->toList();
     }
 
-    // View Lead
-    public function view($id)
-    {
-        $this->request->allowMethod(["get"]);
-
-        // Lead check
-        $element = $this->Leads->get($id, [
-            'contain' => ['Users', 'Clients', 'LeadOffers'],
-        ]);
-
-        $this->set([
-            "status" => true,
-            "message" => "Element",
-            "data" => $element
-        ]);
-
-        $this->viewBuilder()->setOption("serialize", ["status", "message", "data"]);
+    protected function getElement($id, $options = []) {
+        return $this->Leads->get($id, $options);
     }
 
-    // Add lead api
-    public function add()
-    {
-        $this->request->allowMethod(["post"]);
-
-        // form data
-        $formData = $this->request->getData();
-
-        // Check if email address is provided
-        if (empty($formData['email'])) {
-            $status = false;
-            throw new \Exception("Email address is required");
-        } else {
-            // insert new lead
-            $element = $this->Leads->newEmptyEntity();
-
-            $element = $this->Leads->patchEntity($element, $formData);
-
-            if ($this->Leads->save($element)) {
-                // success response
-                $status = true;
-                $message = "Element has been created";
-            } else {
-                // error response
-                $status = false;
-                $message = "Failed to create element";
-            }
-        }
-
-        $this->set([
-            "status" => $status,
-            "message" => $message
-        ]);
-
-        $this->viewBuilder()->setOption("serialize", ["status", "message"]);
+    protected function form($id = null) {
+        $data = $this->request->getData();
+        $element = $id ? $this->getElement($id) : $this->Leads->newEmptyEntity();
+        $element = $this->Leads->patchEntity($element, $data);
+        return $this->Leads->save($element) ? $element : null;
     }
 
-    // Update lead
-    public function edit($id)
-    {
-        $this->request->allowMethod(["put", "post"]);
-
-        $Info = $this->request->getData();
-
-        /* $id = $this->request->getParam("id"); */
-
-        // lead check
-        $element = $this->Leads->get($id);
-
-        if (!empty($element)) {
-            // leads exists
-            $element = $this->Leads->patchEntity($element, $Info);
-
-            if ($this->Leads->save($element)) {
-                // success response
-                $status = true;
-                $message = "Element has been updated";
-            } else {
-                // error response
-                $status = false;
-                $message = "Failed to update element";
-            }
-        } else {
-            // lead not found
-            $status = false;
-            $message = "Element Not Found";
-        }
+    protected function response($data){
 
         $this->set([
-            "status" => $status,
-            "message" => $message
+            'status' => !empty($data),
+            'message' => !empty($data) ? 'Success' : 'Failed',
+            'data' => $data
         ]);
 
-        $this->viewBuilder()->setOption("serialize", ["status", "message"]);
-    }
-
-    // Delete lead api
-    public function delete($id)
-    {
-        $this->request->allowMethod(["delete"]);
-
-        /* $emp_id = $this->request->getParam("id"); */
-
-        $element = $this->Leads->get($id);
-
-        if (!empty($element)) {
-            // lead found
-            if ($this->Leads->delete($element)) {
-                // lead deleted
-                $status = true;
-                $message = "Element has been deleted";
-            } else {
-                // failed to delete
-                $status = false;
-                $message = "Failed to delete element";
-            }
-        } else {
-            // not found
-            $status = false;
-            $message = "Element doesn't exists";
-        }
-
-        $this->set([
-            "status" => $status,
-            "message" => $message
-        ]);
-
-        $this->viewBuilder()->setOption("serialize", ["status", "message"]);
+        $this->viewBuilder()->setOption('serialize', ['status', 'message', 'data']);
     }
 
 }
