299ebb78d065ecd4c7cce6c179ef272653fe9aa2
CarloSalaris <carloa.salaris@gmail.com>
2023-11-23 19:12:23 +0100

    LeadPolicy: registered users can do everything atm

---
+++ src/Policy/LeadPolicy.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Policy;
+
+use App\Model\Entity\Lead;
+use Authorization\IdentityInterface;
+
+/**
+ * Lead policy
+ */
+class LeadPolicy
+{
+    /**
+     * Check if $user can add Lead
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Lead $lead
+     * @return bool
+     */
+    public function canAdd(IdentityInterface $user, Lead $lead)
+    {
+        return true;
+    }
+
+    /**
+     * Check if $user can edit Lead
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Lead $lead
+     * @return bool
+     */
+    public function canEdit(IdentityInterface $user, Lead $lead)
+    {
+        return true;
+    }
+
+    /**
+     * Check if $user can delete Lead
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Lead $lead
+     * @return bool
+     */
+    public function canDelete(IdentityInterface $user, Lead $lead)
+    {
+        return true;
+    }
+
+    /**
+     * Check if $user can view Lead
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Lead $lead
+     * @return bool
+     */
+    public function canView(IdentityInterface $user, Lead $lead)
+    {
+        return true;
+    }
+}
